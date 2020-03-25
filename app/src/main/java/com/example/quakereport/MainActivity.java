8793ffe4a227eae527858d4996ec3b650007bf89
ayooubsmatti <ayooubsmatti@gmail.com>
2020-03-26 00:32:07 +0100

    Parse JSON response in Quake Report app

--- app/src/main/java/com/example/quakereport/MainActivity.java
+++ app/src/main/java/com/example/quakereport/MainActivity.java
@@ -18,14 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("8.2","San Francisco","25 mars ,2020"));
-        earthquakes.add(new Earthquake("6.5","London","25 mars ,2020"));
-        earthquakes.add(new Earthquake("4.7","Tokyo","25 mars ,2020"));
-        earthquakes.add(new Earthquake("4.5","France","25 mars ,2020"));
-        earthquakes.add(new Earthquake("3.3","Mexico","25 mars ,2020"));
-        earthquakes.add(new Earthquake("4.5","Rio de Janeiro","25 mars ,2020"));
-
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
         ListView earthquikelistView = (ListView) findViewById(R.id.list);
         EarthquakeAdapter adapter = new EarthquakeAdapter(this,earthquakes);
         earthquikelistView.setAdapter(adapter);
